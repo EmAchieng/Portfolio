5832f05d78fe12b828888fcd2980ceced48809ef
EmAchieng <emillyachieng@gmail.com>
2020-05-05 18:13:19 +0900

    jobs model

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    #images
+    Image = models.ImageField(upload_to='images/')
+    #summary
+    Summary = models.CharField(max_length =200)

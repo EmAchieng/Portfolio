9d5ca7a883631538295e1e9781e43ee312f9b4b6
EmAchieng <emillyachieng@gmail.com>
2020-05-07 17:56:39 +0900

    collectstatic

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -24,4 +24,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emily', jobs.views.emily, name='emily' ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

590f5c6b1d9eef26e69716d9d827ad94579e520f
EmAchieng <emillyachieng@gmail.com>
2020-06-25 18:59:31 +0900

    added host

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jy^bqj+urr30zrai6ra2mncct1i))bnk0u_&2^9nppl#pq1#5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['emachieng.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

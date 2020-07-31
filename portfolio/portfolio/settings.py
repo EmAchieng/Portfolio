400d0cb8d08fdc9125ac2211267cccc91b0ae9bc
heyscam <heyscam@gmail.com>
2020-07-31 14:33:59 +0100

    Add dj_database_url

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,16 +88,8 @@ if ENV == "local":
         }
     }
 else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "portfoliodb",
-            "USER": "postgres",
-            "PASSWORD": "django123",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
-    }
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES["default"].update(db_from_env)
 
 
 # Password validation

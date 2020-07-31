309a0979b5e33963ccfaf63a00591adf47034628
heyscam <heyscam@gmail.com>
2020-07-31 14:35:21 +0100

    Fix incorrect dj_database_url logic

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -80,14 +80,15 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if ENV == "local":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
-else:
+}
+
+
+if ENV == "production":
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES["default"].update(db_from_env)
 
